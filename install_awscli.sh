c68ed2ab1634a1c5755ac1ee16b5ed976bc5da8a
jinal--shah <jnshah@gmail.com>
2017-02-01 18:05:17 +0000

    adding ssl trust certs for downloading pip. But this time, um, actually doing it.

--- install_awscli.sh
+++ install_awscli.sh
@@ -2,7 +2,7 @@
 # vim: et smartindent sr sw=4 ts=4:
 APK_TMP=/var/cache/apk
 GET_PIP=https://bootstrap.pypa.io/get-pip.py
-BUILD_PKGS="openssl-dev wget python-dev"
+BUILD_PKGS="openssl-dev wget python-dev ca-certificates"
 PKGS="$BUILD_PKGS python groff less"
 echo "INFO $0: installing python, pip and awscli"
 apk --no-cache add --update $PKGS                  \
