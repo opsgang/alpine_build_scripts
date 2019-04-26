854c26a40e664f5fcbba7ce7eb0aa2eaa17ee5f4
Fabien Delpierre <fabien.delpierre@gmail.com>
2019-04-26 11:30:36 -0400

    upgrade to Python 3.x for AWS CLI

--- install_awscli.sh
+++ install_awscli.sh
@@ -2,12 +2,12 @@
 # vim: et smartindent sr sw=4 ts=4:
 APK_TMP=/var/cache/apk
 GET_PIP=https://bootstrap.pypa.io/get-pip.py
-BUILD_PKGS="openssl-dev wget python-dev ca-certificates"
-PKGS="$BUILD_PKGS python groff less"
+BUILD_PKGS="openssl-dev wget python3-dev ca-certificates"
+PKGS="$BUILD_PKGS python3 groff less"
 echo "INFO $0: installing python, pip and awscli"
 apk --no-cache add --update $PKGS                  \
 && wget -q -T 10 -O /var/tmp/get-pip.py $GET_PIP   \
-&& python /var/tmp/get-pip.py                      \
+&& python3 /var/tmp/get-pip.py                     \
 && pip --no-cache-dir install --upgrade awscli     \
 && apk --no-cache --purge del --update $BUILD_PKGS \
 && rm -rf /var/tmp/get-pip.py $APK_TMP/*           \
